bea20ee67d86c1540ad83ba4990fbb3626a8514b
happyy07 <71673923+happyy07@users.noreply.github.com>
2025-01-19 15:37:14 -0500

    Update payment.service.ts

--- src/app/services/payment.service.ts
+++ src/app/services/payment.service.ts
@@ -8,7 +8,7 @@ import { map } from 'rxjs/operators';
   providedIn: 'root'
 })
 export class PaymentService {
-  private apiUrl = 'https://76a7-74-12-114-233.ngrok-free.app'; // adjust based on your backend URL
+  private apiUrl = 'https://payment-backend-management.vercel.app'; // adjust based on your backend URL
 
   constructor(private http: HttpClient) {}
 
